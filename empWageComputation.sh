04807af360b76a27f3032387653659ac924d151b
Viresh1994 <vireshanadakatni@gmail.com>
2022-10-12 09:08:00 +0530

    [Viresh] Employee Part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,22 @@
 #! /bin/bash/ -x
 
 echo "Welcome to emp wage calculation"
-attendance=$((RANDOM%2))
+attendance=$((RANDOM%3))
 isPresent=1
 
+isPartTime=2
 wagePerHour=20
 fullDayHour=8
+isPartTimeHr=4
 totalPay=0
 if [ $attendance -eq $isPresent ]
 then
 	echo "Present"
 	totalPay=$(($wagePerHour * $fullDayHour))
+elif  [ $attendance -eq $isPartTime ]
+then
+	echo "Present in Part time"
+	totalPay=$(($wagePerHour * $isPartTimeHr))
 else
 	echo "Absent"
 fi
