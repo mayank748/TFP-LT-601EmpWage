a0478318cd902fb4f3183b3c460f2d19f9cb0f3f
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-12 08:49:58 +0530

    [Prashik]refactor the code to calculate empWagePerDay

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,18 @@
+#! /bin/bash/ -x
+
 echo "Welcome to emp wage calculation"
 attendance=$((RANDOM%2))
 isPresent=1
+
+wagePerHour=20
+fullDayHour=8
+totalPay=0
 if [ $attendance -eq $isPresent ]
 then
-echo "Present"
+	echo "Present"
+	totalPay=$(($wagePerHour * $fullDayHour))
 else
-echo "Absent"
+	echo "Absent"
 fi
+
+echo $totalPay
